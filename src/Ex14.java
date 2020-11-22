75d0053a10466e340205c6b00f0b2e9c6184b1d5
dorsamoha <dorsamoha@gmail.com>
2020-11-22 19:23:03 +0200

    first commit mmn14

---
+++ src/Ex14.java
@@ -0,0 +1,38 @@
+/**
+ * Solution for mmn 14
+ *
+ * @author Dor Samoha
+ * ID: 312503287
+ * @version Nov 22nd, 2020
+ */
+
+public class Ex14 {
+
+    /**
+     * Find single int.
+     */
+    public static int findSingle(int[] a) {
+        return 0;
+    }
+
+    /**
+     * Smallest sub sum int.
+     */
+    public static int smallestSubSum(int arr[], int x) {
+        return 0;
+    }
+
+    /**
+     * Solutions int.
+     */
+    public static int solutions(int num) {
+        return 0;
+    }
+
+    /**
+     * The entry point of application.
+     */
+    public static void main(String[] args) {
+
+    }
+}
